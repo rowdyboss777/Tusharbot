47b4846331855ae244739e5375b12c76470dd5cf
ROWDY <harrykashy53@gmail.com>
2025-04-01 12:40:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -682,7 +682,7 @@ async def upload(bot: Client, m: Message):
         name_message = await bot.send_message(
             m.chat.id,
         
-            f"🔖 {fancy_batch_name}"
+            f""<blockquate><code>🔖 {fancy_batch_name}</code><blockquote></blocquote>"
             
         )
         await bot.pin_chat_message(m.chat.id, name_message.id)
